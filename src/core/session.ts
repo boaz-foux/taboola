97ef4aec0d70001b72b0ab6aa810285cbb79c712
Boaz Foux <boaz.f@taboola.com>
2022-04-18 12:53:13 +0300

    with mock data

--- src/core/session.ts
+++ src/core/session.ts
@@ -1,5 +1,6 @@
 import { isMobile } from "../utils";
 
+
 export class Session {
     static apikey: string = '';
     static id: number = -1;
@@ -7,7 +8,7 @@ export class Session {
         return Session.id > 0 && Session.apikey;
     }
     static async fetch(settings: { count: number }) {
-        if(!Session.valid){
+        if (!Session.valid) {
             throw new Error('invalid session, could not pull data');
         }
         const base = 'https://api.taboola.com/1.0/json/taboola-templates/recommendations.get';
@@ -20,7 +21,13 @@ export class Session {
         ].map(([key, value]) => `${key}=${value}`).join('&')
         const url = `${base}?${parameters}`;
         const response = await fetch(url);
-        return (await response.json())['list'];
+        let list = (await response.json())['list'];
+        if (!(list?.length)) {
+            const mock = await import("./mock.json");
+            const mockData = [].concat(mock.list).sort(() => Math.random());
+            list = Array(settings.count).fill(0).map((x, i) => mockData[i % mockData.length]);
+        }
+        return list;
     }
 }
 
