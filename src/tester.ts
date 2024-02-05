f5de3fe8a80ffae28b546abedece4e9444fbc1ec
Boaz Foux <boaz.f@taboola.com>
2024-02-05 23:43:26 +0200

    minor

--- src/tester.ts
+++ src/tester.ts
@@ -1,8 +1,8 @@
-import taboola from './taboola';
 import Base from './core/base';
 import { inject } from './utils';
-
-setTimeout(() => {
+const promise=  import('./taboola');
+setTimeout(async () => {
+    const taboola = (await promise).default;
     taboola.init({
         apikey: "f9040ab1b9c802857aa783c469d0e0ff7e7366e4",
         id: 214321562187
